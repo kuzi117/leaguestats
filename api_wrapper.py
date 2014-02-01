1a31bd03516a24a9dea7be5dbb85fc8d43118a31
kuzi117 <puckmaster_84@hotmail.com>
2014-01-31 23:09:12 -0700

    Rewritten api_wrapper.

--- api_wrapper.py
+++ api_wrapper.py
@@ -1,137 +1,130 @@
-import file_wrapper
+from util import dbg_str
+from util import base_url
+from util import key
 
-import requests as reqs
+GENERIC_ERROR = 0
+NOT_FOUND = 1
+CLIENT_ERROR = 2
+SERVER_ERROR = 3
 
-import datetime
-from time import sleep
-
-# Application varaibles
-debug = False
-dbg_str = 'DEBUG: '
-files = file_wrapper.FileWrapper(debug = debug)
-request_queue = []
-
-# Riot API variables
-key = 'b327abf7-c1ef-4b97-acce-5528d97d1437'
-base_url = 'https://prod.api.pvp.net/api/lol/na/v{}/'
-
-def _get(version, url, payload = {}):
+def _get(region, version, url, payload={}, **args):
     """
     Generic get from Riot.
-    """
-    # Clean the queue out
-    now = datetime.datetime.now()
-    while (len(request_queue) > 0 and
-            (now-request_queue[0]).total_seconds() > 10):
-        request_queue.pop(0)
     
-    # Sleep for the remainder of the ten seconds on the earliest request
-    if len(request_queue) >= 10:
-        wait = 10-(now-request_queue[0]).total_seconds()
-        if debug:
-            print(dgb_str + 'WAITING ON NEXT AVAILABLE REQUEST ({}s)'
-                    .format(wait))
-        sleep(wait)
-        request_queue.pop(0)
-        
-    url = base_url.format(version) + url
+    Returns an error code or the recieved object already converted to
+    usable form.
+    """
+    url = base_url.format(region, version) + url
     
     # Print URL if debug is on
     if debug:
         print(dbg_str + 'REQUEST URL: ' + url)
-    
     payload['api_key'] = key
-    
+
     # Request
-    request_queue.append(datetime.datetime.now())
     r = reqs.get(url, params = payload)
     
-    if debug:
+    if args.get('debug', False):
         print(dbg_str + 'RESPONSE STATUS CODE: {}'.format(r.status_code))
     
     # All good
     if r.status_code == 200:
-        return r
-    # Error code
+        return r.json()
+        
+    # Error codes
+    elif r.status_code == 400:
+        if args.get('debug', False):
+            print(dbg_str + 'Internal error; bad request generated!')
+        return CLIENT_ERROR
+    
+    elif r.status_code == 401:
+        if args.get('debug', False):
+            print(dbg_str + 'Unauthorized request; bad key?')
+        return CLIENT_ERROR
+        
+    elif r.status_code == 404:
+        if args.get('debug', False):
+            print(dbg_str + '{} not found on server!'
+                .format(args.get('not_found', 'Requested object')))
+        return NOT_FOUND
+        
+    elif r.status_code == 429:
+        if args.get('debug', False):
+            print(dbg_str + 'Too many requests!')
+        return GENERIC_ERROR
+    
+    elif r.status_code == 500:
+        if args.get('debug', False):
+            print(dbg_str + 'Server error; generic error response!')
+        return SERVER_ERROR
+    
+    elif r.status_code == 503:
+        if args.get('debug', False):
+            print(dbg_str + 'Server error; service unavailable!')
+        return SERVER_ERROR
+    
     else:
-        if debug and r.status_code == 429:
-            print(dbg_str + "TOO MANY REQUESTS!")
-        return r.status_code
+        if args.get('debug', False):
+            print(dbg_str + 'UNKNOWN STATUS CODE {} RETURNED!'
+                    .format(r.status_code))
+        return GENERIC_ERROR
 
-def get_summoner_by_name(name, force_reload=False):
-    # Ensure name entered wasn't empty
-    if name == None:
-        return
+class APIWrapper:
+    def __init__(self, **args):
+        # Default things
+        self.debug = args.get('debug', False)
     
-    # Request locally
-    file_result = files.get_summoner_by_name(name)
-    if file_result != None and not force_reload:
-        if debug:
-            print(dbg_str + 'FOUND SUMMONER IN LOCAL CACHE')
-            
-        return file_result
-    
-    result = _get(1.2, 'summoner/by-name/{}'.format(name))
-    
-    # Notify user on HTTP error codes
-    if type(result) == int:
-        if result == 404:
+    def sum_by_name(self, name, region):
+        """
+        Returns summoner object by name, or None if the couldn't be
+        found or if another error occurs.
+        """
+        if name == None or type(name) != str:
+            if self.debug:
+               print(dbg_str + 'Bad summoner name.')
+            return None
+        
+        result = _get(region, 1.3, 'summoner/by-name/{}'.format(name),
+                        debug = self.debug)
+        
+        if result == NOT_FOUND:
             print('That name couldn\'nt be found on this server.')
-        elif result == 400:
-            print('An internal error occured while contacting '
-                   'the server!')
-        elif result == 500:
-            print('Servers are experiencing difficulties, try again.')
+            return None
+        elif result == CLIENT_ERROR:
+            print('There was a client error requesting the summoner.')
+            return None
+        elif result == SERVER_ERROR:
+            print('There was a server error requesting the summoner. '
+                   'Try again later?')
+            return None
+        elif result == GENERIC_ERROR:
+            print('An unkown error occured. Try again later?')
+            return None
         else:
-            print('An unknown error occured! ({})'.format(result))
-        
-        # All of these return no user found
-        return None
-    else:
-        # Convert from json format to dict
-        result = result.json()
-        
-        # Save to local
-        files.save_summoner(result)
-        
-        return result
-
-def get_recent_games(sumid, force_reload=False):
-    """
-    Gets recent games by summoner id.
-    """
-    # Make sure summoner id
-    if sumid == None:
-        return None
-        
-    # Request locally
-    file_result = files.get_recent_games(sumid)
-    if file_result != None and not force_reload:
-        if debug:
-            print(dbg_str + 'FOUND GAMES IN LOCAL CACHE')
+            return result
             
-        return file_result
-    
-    result = _get(1.3, 'game/by-summoner/{}/recent'.format(sumid))
-    
-    # Notify user on HTTP error codes
-    if type(result) == int:
-        if result == 404:
-            print('That name couldn\'t be found on this server.')
-        elif result == 400:
-            print('An internal error occured while contacting '
-                   'the server!')
-        elif result == 500:
-            print('Servers are experiencing difficulties, try again.')
+    def get_recent_games(sumid, force_reload=False):
+        """
+        Gets recent games by summoner id.
+        """
+        # Make sure summoner id
+        if sumid == None or type(sumid) != int:
+            return None
+        
+        result = _get(1.3, 'game/by-summoner/{}/recent'.format(sumid))
+        
+        if result == NOT_FOUND:
+            print('That name couldn\'nt be found on this server.')
+            return None
+        elif result == CLIENT_ERROR:
+            print('There was a client error requesting recent games.')
+            return None
+        elif result == SERVER_ERROR:
+            print('There was a server error requesting recent games. '
+                   'Try again later?')
+            return None
+        elif result == GENERIC_ERROR:
+            print('An unkown error occured. Try again later?')
+            return None
         else:
-            print('An unknown error occured! ({})'.format(result))
-    else:
-        # Convert from json format to dict
-        result = result.json()
-        
-        # Save to local
-        files.save_recent_games(result)
-        
-        return result
-    
-    return result.json()
+            return result
