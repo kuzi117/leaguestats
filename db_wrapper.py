6ab7d93e45f1247f67e1eb64d7aff32083f8134d
kuzi117 <puckmaster_84@hotmail.com>
2014-01-31 23:12:00 -0700

    Added create_table, row_exists and changed table_exists.

--- db_wrapper.py
+++ db_wrapper.py
@@ -8,11 +8,11 @@ class DBWrapper():
     Class to wrap an SQLite db for stats storing.
     """
     
-    def __init__(self, debug = False):
-        self.debug = debug
+    def __init__(self, **args):
+        self.debug = args.get('debug', False)
         self.conn = sqlite3.connect(file_path + '/stats.db')
     
-    def close(self):
+    def exit(self):
         """
         Prepares for exit.
         """
@@ -21,21 +21,21 @@ class DBWrapper():
         
         self.conn.close()
         
-    def does_table_exist(self, table_name):
+    def table_exists(self, table_name):
         """
         Returns whether or not a table exists in the database.
         """
+        # Prep the statement and symbols
+        stmt = ('SELECT name '
+                'FROM sqlite_master '
+                'WHERE type=\'table\' AND name=?;')
+        symbols = (table_name,)
+        
         # Get DB cursor
         cursor = self.conn.cursor()
         
-        # Prep the statement
-        symbols = (table_name,)
-        statement = ('SELECT name '
-                     'FROM sqlite_master '
-                     'WHERE type=\'table\' AND name=?;')
-        
         # Get results
-        cursor.execute(statement, symbols)
+        cursor.execute(stmt, symbols)
         one = cursor.fetchone()
         cursor.close()
         
@@ -43,3 +43,60 @@ class DBWrapper():
             return True
         else:
             return False
+    
+    def row_exists(self, table_name, row_name):
+        """
+        Returns whether or not a row exists in a table.
+        """
+        # Prep statement and symbols
+        stmt = ('SELECT ? '
+                'FROM ? ')
+        symbols = (table_name, row_name)
+        
+        # Get DB cursor
+        cursor = self.conn.cursor()
+        
+        # Get results
+        cursor.execute(stmt, symbols)
+        one = cursor.fetchone()
+        cursor.close()
+        
+        if one != None:
+            return True
+        else:
+            return False
+        
+
+    def create_table(self, name, field_names, field_types, primary_name):
+        """
+        Creates a table.
+        """
+        # Ensure that all fields are typed
+        if len(field_types) != len(field_names):
+            raise AssertionError('Field types length doesn\'t match '
+                                  ' field names length!')
+        # Ensure that primary field is there
+        elif primary_name not in field_names:
+            raise AssertionError('Primary name not in field names!')
+        
+        # Create statment
+        stmt = ('CREATE TABLE ? ('
+                '{}'
+                'PRIMARY KEY (?))')
+        stmt= stmt.format('? ?,' * len(field_names))
+        
+        # Create symbols list for formatting
+        symbols = [name]
+        
+        for i in range(len(field_names)):
+            symbols += [field_types[i]]
+            symbols += [field_names[i]]
+        
+        symbols += [primary_name]
+        
+        curs = self.conn.cursor()
+        
+        curs.execute(stmt, symbols)
+        
+        curs.close()
+    
