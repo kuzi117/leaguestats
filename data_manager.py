5eb8c34304004d4f9efa9f954e7e5d6bf3ba228a
kuzi117 <puckmaster_84@hotmail.com>
2014-02-02 01:04:24 -0700

    Added summoner_by_name, save_summoners

--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,4 @@
-from util import Singleton
-from util import dbg_str
+from util import Singleton, dbg_str
 import db_wrapper
 
 class DataManager(metaclass=Singleton):
@@ -23,11 +22,60 @@ class DataManager(metaclass=Singleton):
         """
         self.db.exit()
     
-    def sum_by_name(self, name, region):
+    def summoner_by_name(self, name, region):
         """
-        Gets a summoner by name from the database.
+        Gets a summoner by name from the database. Returns only a single
+        summoner.
         """
-        pass
+        name = name.lower().replace(' ', '')
+        rows = self.db.select_values('summoners',
+                                     ['*'],
+                                     ['standard_name like \'{}\''
+                                     .format(name)])
+        if rows:
+            # Drop the cache date and standardized name from the end of 
+            # the result so it's like riot's result
+            rows = [
+                {'id': x[0],
+                 'name': x[1],
+                 'summonerLevel': x[2],
+                 'profileIconId': x[3],
+                 'revisionDate': x[4]
+                    }
+                     for x in rows]
+                     
+            if self.debug:
+                print(dbg_str + 'Summoners from db: {}'.format(rows))
+            
+            return rows[0]
+        else:
+            return None
+    
+    ### Save functions
+    def save_summoners(self, summoners):
+        """
+        Saves summoners to the database. 
+        Expects summoners to be a list of SummonerDto.
+        """
+        values = []
+        for summoner in summoners:
+            val_list = []
+            
+            # Append all values in order.
+            val_list.append(summoner['id'])
+            val_list.append('\'{}\''.format(summoner['name']))
+            val_list.append(summoner['summonerLevel'])
+            val_list.append(summoner['profileIconId'])
+            val_list.append(summoner['revisionDate'])
+                            
+            # Add cache date and standardized name
+            val_list.append('\'{}\''.format(summoner['name']
+                                .lower().replace(' ', '')))
+            val_list.append('datetime(\'now\')')
+            
+            values.append(tuple(val_list))
+        
+        self.db.insert_values('summoners', values)
         
     ### Setup functions
     def setup_summoners(self):
@@ -42,12 +90,14 @@ class DataManager(metaclass=Singleton):
                  'summonerLevel',
                  'profileIconId',
                  'revisionDate',
+                 'standard_name',
                  'cacheDate')
         types = ('integer',
                  'text',
                  'integer',
                  'integer',
                  'integer',
+                 'text',
                  'integer')
         
         self.db.create_table('summoners', names, types, 'id')
